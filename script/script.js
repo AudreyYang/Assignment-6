be48326d441cb6c5f2331396a2baf04dd5c51a80
AudreyYang <loaudreyyangve@gmail.com>
2015-12-02 22:30:17 -0500

    ah

--- script/script.js
+++ script/script.js
@@ -22,11 +22,14 @@ var popByState = d3.map();
 var scaleR = d3.scale.sqrt().range([5,130]),
     scaleColor = d3.scale.linear().domain([70,90]).range(['white','red']);
 
+
 //import data
 queue()
 	.defer(d3.json, "data/gz_2010_us_040_00_5m.json")
     .defer(d3.csv, "data/2014_state_pop.csv", parseData)
 	.await(function(err, states, pop){
+        console.log(states);
+        console.log(pop);
 
         //mine data to complete scales
         var maxPop = d3.max(pop);
@@ -34,7 +37,7 @@ queue()
 
         //construct a new array of data
         var data = states.features.map(function(d){
-            var centroid = path.centroid(d); //provides two numbers [x,y] indicating the screen coordinates of the state
+            var centroid = path.centroid(d);
 
             return {
                 fullName:d.properties.NAME,
@@ -42,15 +45,15 @@ queue()
                 x0:centroid[0],
                 y0:centroid[1],
                 x:centroid[0],
-                y:centroid[1]
+                y:centroid[1],
+                r: scaleR((popByState.get(d.properties.STATE)).pop)
             }
         });
         console.log(data);
 
 		var nodes = map.selectAll('.state')
             .data(data, function(d){return d.state});
-
-        //Represent as a cartogram of populations
+        
         var nodesEnter = nodes.enter()
             .append('g')
             .attr('class','state');
@@ -74,19 +77,77 @@ queue()
         nodes
             .append('text')
             .text(function(d){
-                return d.name;
+                return d.fullName;
             })
             .attr('text-anchor','middle');
 
         //TODO: create a force layout
         //with what physical parameters?
-        var force = d3.layout.force()
+        var force = d3.layout.force();
         //on "tick" event ...
+        force
+            .size([width,height])
+            .charge(0)
+            .gravity(0)
+            .nodes(data)
+            .on('tick', onTick)
+            .start();
 
-	});
+        function onTick(e){
+            var q = d3.geom.quadtree(data),
+                i = 0,
+                n = data.length;
+
+            while(++i<n){
+                q.visit(collide(data[i]));
+            }
+
+            nodes
+                .each(gravity(e.alpha *.1))
+                .attr('cx',function(d){return d.x})
+                .attr('cy',function(d){return d.y})
+                .attr('transform',function(d){
+                    return 'translate('+d.x+','+d.y+')';
+                });
+
+            //custom gravity
+            function gravity(k){
+                return function(d){
+                    d.y += (d.y0 - d.y)*k;
+                    d.x += (d.x0 - d.x)*k;
+                }
+            }
+
+            function collide(dataPoint){
+                var nr = dataPoint.r + 5,
+                    nx1 = dataPoint.x - nr,
+                    ny1 = dataPoint.y - nr,
+                    nx2 = dataPoint.x + nr,
+                    ny2 = dataPoint.y + nr;
+
+                return function(quadPoint,x1,y1,x2,y2){
+                    if(quadPoint.point && (quadPoint.point !== dataPoint)){
+                        var x = dataPoint.x - quadPoint.point.x,
+                            y = dataPoint.y - quadPoint.point.y,
+                            l = Math.sqrt(x*x+y*y),
+                            r = nr + quadPoint.point.r;
+                        if(l<r){
+                            l = (l-r)/l*.1;
+                            dataPoint.x -= x*= (l*.05);
+                            dataPoint.y -= y*= l;
+                            quadPoint.point.x += (x*.05);
+                            quadPoint.point.y += y;
+                        }
+                    }
+                    return x1>nx2 || x2<nx1 || y1>ny2 || y2<ny1;
+                }
+            }
+
+        }
+
+    });
 
 function parseData(d){
-    //Use the parse function to populate the lookup table of states and their populations/% pop 18+
 
     popByState.set(d.STATE,{
         'pop':+d.POPESTIMATE2014,
